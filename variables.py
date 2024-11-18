1b92075d3b3bfdc5d372a5dff80b99ee967951f4
Risbinrh <risbin.rh@adyog.in>
2024-11-18 16:06:35 +0530

    sample pgm and variables are commited

---
+++ variables.py
@@ -0,0 +1,24 @@
+
+# Variables in Python are containers or labels that store data values.
+#  They are fundamental building blocks in programming that allow you to store and manipulate information.
+
+name = "risbin" # string 
+age = 22         # integer
+is_adult = True # boolean
+price = 22.22 # float
+number = 1 + 2j # complex number
+number = [1,2,3] # list
+number = (1,2,3) # tuple
+
+print("hai may name is ",name , "and my age is ", age , "my watch price is ",price)
+
+
+
+# Single assignment
+x = 10
+
+# Multiple assignment
+a, b, c = 1, 2, 3
+
+# Same value to multiple variables
+x = y = z = 0
